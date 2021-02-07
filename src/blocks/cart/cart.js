51e4ca49dd2c9fc7567f7f75360d5a77f89925a2
edward <edward@kali>
2021-02-08 02:44:40 +0300

    adaptive sidebar on page details

--- src/blocks/cart/cart.js
+++ src/blocks/cart/cart.js
@@ -11,19 +11,38 @@
       navClass: ['cart__slider-btn-left', 'cart__slider-btn-right']
    });
 
+   $('.cart__right-slider.one').owlCarousel({
+      loop: true,
+      items: 1,
+      margin: 0,
+      dots: false,
+      nav: true,
+      navText: ['', ''],
+      navContainer: '.cart__right-slider.one',
+      navClass: ['cart__left-slider-btn', 'cart__right-slider-btn']
+   });
+   $('.cart__right-slider.two').owlCarousel({
+      loop: true,
+      items: 1,
+      margin: 0,
+      dots: false,
+      nav: true,
+      navText: ['', ''],
+      navContainer: '.cart__right-slider.two',
+      navClass: ['cart__left-slider-btn', 'cart__right-slider-btn']
+   });
+   $('.cart__right-slider.three').owlCarousel({
+      loop: true,
+      items: 1,
+      margin: 0,
+      dots: false,
+      nav: true,
+      navText: ['', ''],
+      navContainer: '.cart__right-slider.three',
+      navClass: ['cart__left-slider-btn', 'cart__right-slider-btn']
+   })
+
 })();
 
 
 
-// (() => {
-//    $('.cart__right-slider').owlCarousel({
-//       loop: true,
-//       items: 1,
-//       margin: 0,
-//       dots: false,
-//       nav: true,
-//       navText: ['', ''],
-//       navContainer: '.cart__right-slider',
-//       navClass: ['cart__right-slider-btn', 'cart__right-slider-btn']
-//    })
-// })();
--- src/blocks/filter/filter.js
+++ src/blocks/filter/filter.js
@@ -1,28 +1,31 @@
 (() => {
 	// Если элементов больше пяти - лишние спрятать
-	$('.filter__items').each(function() {
+	$('.filter__items').each(function () {
 		let $el = $(this).find('.filter__item');
 
-		if($el.length > 4) {
+		if ($el.length > 4) {
 			$(this).append("<span class='filter__more'>показать все</span>");
 
-			$el.each(function(index) {
-				if(index > 3)
-					$(this).css({display: "none"});
+			$el.each(function (index) {
+				if (index > 3)
+					$(this).css({ display: "none" });
 			});
 		}
 	});
 
 	// Показать спрятанные и убрать кнопку
-	$('.filter').on('click', '.filter__more', function() {
+	$('.filter').on('click', '.filter__more', function () {
 		$(this)
 			.parent('.filter__items')
 			.find('.filter__item')
-			.css({display: 'block'})
+			.css({ display: 'block' })
 			.end().end()
 			.remove();
 	});
-	
 
+	$('.cart__sidebar-mobile').on('click', function (e) {
+		e.preventDefault();
+		$('.cart__sidebar').toggleClass('cart__sidebar-active');
+	})
 
 })();

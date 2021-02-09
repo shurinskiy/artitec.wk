8d8781796f0bfcf00b903424730a7522cd5ae74d
_tango <shurinskiy@mail.ru>
2021-02-09 19:06:47 +0600

    card buttons fix

--- src/blocks/choice/choice.js
+++ src/blocks/choice/choice.js
@@ -11,14 +11,20 @@ import 'owl.carousel';
 		navContainer: '.choice__items',
 		navClass: ['choice__prev','choice__next'],
 		responsive:{
-			960:	{ 
+			1100:	{ 
 				items: 5,
 			},
-			640:	{ 
+			960:	{ 
 				items: 4,
 			},
-			0:	{ 
+			740:	{ 
 				items: 3,
+			},
+			500:	{ 
+				items: 2,
+			},
+			0:	{ 
+				items: 1,
 			}
 		}
 	});
---
+++ src/blocks/header/header.js
@@ -0,0 +1,22 @@
+(() => {
+	
+	$menu = $('.header__side-top');
+
+	$('.header__menu-open, .header__menu-close').on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		$menu.toggleClass('opened');
+	})
+
+	$(window).on('click', function(e) {
+		let target = e.target.classList;
+
+		if($menu.hasClass('opened') && 
+			!target.contains('header__tel') && 
+			!target.contains('header__social-item') && 
+			!target.contains('header__navi-item')) {
+				$menu.toggleClass('opened');
+			}
+	});
+
+})();
